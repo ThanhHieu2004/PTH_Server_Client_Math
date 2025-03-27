9204ccd74824f272e32371a47797a0eb00d521ef
Thanh Hieu <thanhhieu8304@gmail.com>
2025-03-27 14:13:08 +0700

    Modified Client.java to use Railway Proxy TCP

--- src/socketModules/Client.java
+++ src/socketModules/Client.java
@@ -24,11 +24,11 @@ public class Client {
 		String ex_prompt = "";
 		String input, input2;
 		try {
-//			client = new Socket("localhost", 2025);
-			String host = "pthserverclientmath-production.up.railway.app";
-			int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+			// client = new Socket("localhost", 2025);
+			// String host = "pthserverclientmath-production.up.railway.app";
+			// int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
 			// client = new Socket(host, port);
-			client = new Socket("pthserverclientmath-production.up.railway.app", 8080);
+			client = new Socket("hopper.proxy.rlwy.net", 32418);
 			DataInputStream dis = new DataInputStream(client.getInputStream()); 
 			DataOutputStream dos = new DataOutputStream(client.getOutputStream());
 			Scanner sc = new Scanner(System.in);
